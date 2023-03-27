53a3af7b7ee5ff1545f2bae7550a105bee39a970
Rock N Rome <126816805+rocknrome@users.noreply.github.com>
2023-03-27 12:12:15 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+//to show a name and first letter of a last name, like on a banking app
+var firstName = 'Roman Ravilevich';
+var lastName = 'Larionov';
+var firstLetterOfLastName = lastName[0];
+console.log('Greetings ' + (firstName) + ' ' + (firstLetterOfLastName) + '.');
